84c4eddbc5033eb2eb00eb1b8d7c7d0ee1da71ca
996458765@qq.com <996458765@qq.com>
2021-07-08 01:54:30 +0800

    首个页面

--- src/pages/MyTableList/index.tsx
+++ src/pages/MyTableList/index.tsx
@@ -1,57 +1,109 @@
-// import React from 'react'
-// import { PageContainer } from '@ant-design/pro-layout';
-// import { Button } from 'antd';
-
-
-// const MyTableList: React.FC = () => {
-
-//     return (
-//         <PageContainer>
-//             content="欢迎使用 ProLayout 组件"
-//             tabList={[
-//                 {
-//                 tab: '基本信息',
-//                 key: 'base',
-//                 },
-//                 {
-//                 tab: '详细信息',
-//                 key: 'info',
-//                 },
-//             ]}
-//             extra={[
-//                 <Button key="3">操作</Button>,
-//                 <Button key="2">操作</Button>,
-//                 <Button key="1" type="primary">
-//                 主操作
-//                 </Button>,
-//             ]}
-//             footer={[
-//                 <Button key="rest">重置</Button>,
-//                 <Button key="submit" type="primary">
-//                 提交
-//                 </Button>,
-//             ]}
-
-//         </PageContainer>
-
-//     )
-// }
-
-// export default MyTableList
-
-
 import React from 'react';
 import { EllipsisOutlined } from '@ant-design/icons';
 import { Button, Dropdown, Menu } from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
 import ProCard from '@ant-design/pro-card';
+// import ProForm, { ProFormText, ProFormDateRangePicker, ProFormSelect } from '@ant-design/pro-form';
+import { Form, Input, Checkbox } from 'antd';
+import ProTable from '@ant-design/pro-table';
+import type { ProColumns } from '@ant-design/pro-table';
+
+const onFinish = (values: any) => {
+  // eslint-disable-next-line no-console
+  console.log('Success:', values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  // eslint-disable-next-line no-console
+  console.log('Failed:', errorInfo);
+};
+
+
+const valueEnum = {
+  0: 'close',
+  1: 'running',
+  2: 'online',
+  3: 'error',
+};
+
+export type TableListItem = {
+  key: number;
+  name: string;
+  containers: number;
+  creator: string;
+  status: string;
+  createdAt: number;
+  memo: string;
+};
+const tableListDataSource: TableListItem[] = [];
+
+const creators = ['付小小', '曲丽丽', '林东东', '陈帅帅', '兼某某'];
+
+for (let i = 0; i < 50; i += 1) {
+  tableListDataSource.push({
+    key: i,
+    name: 'AppName',
+    containers: Math.floor(Math.random() * 20),
+    creator: creators[Math.floor(Math.random() * creators.length)],
+    status: valueEnum[Math.floor(Math.random() * 10) % 4],
+    createdAt: Date.now() - Math.floor(Math.random() * 100000),
+    memo: i % 2 === 1 ? '很长很长很长很长很长很长很长的文字要展示但是要留下尾巴' : '简短备注文案',
+  });
+}
+
+const columns: ProColumns<TableListItem>[] = [
+  {
+    title: '应用名称',
+    // width: 80,
+    dataIndex: 'name',
+    // render: (_) => <a>{_}</a>,
+  },
+  {
+    title: '容器数量',
+    dataIndex: 'containers',
+    // align: 'center',
+    sorter: (a, b) => a.containers - b.containers,
+  },
+  {
+    title: '状态',
+    // width: 80,
+    dataIndex: 'status',
+    initialValue: 'all',
+    valueEnum: {
+      all: { text: '全部', status: 'Default' },
+      close: { text: '关闭', status: 'Default' },
+      running: { text: '运行中', status: 'Processing' },
+      online: { text: '已上线', status: 'Success' },
+      error: { text: '异常', status: 'Error' },
+    },
+  },
+  {
+    title: '创建者',
+    // width: 80,
+    dataIndex: 'creator',
+    valueEnum: {
+      all: { text: '全部' },
+      付小小: { text: '付小小' },
+      曲丽丽: { text: '曲丽丽' },
+      林东东: { text: '林东东' },
+      陈帅帅: { text: '陈帅帅' },
+      兼某某: { text: '兼某某' },
+    },
+  },
+  {
+    title: '备注',
+    dataIndex: 'memo',
+    ellipsis: true,
+    copyable: true,
+  },
+];
 
 export default () => (
-  <div
-    style={{
-      background: '#F5F7FA',
-    }}
-  >
+  // <div
+  //   style={{
+  //     background: '#F5F7FA',
+  //   }}
+  // >
     <PageContainer
       header={{
         title: '页面标题',
@@ -109,7 +161,6 @@ export default () => (
       tabProps={{
         type: 'editable-card',
         hideAdd: true,
-        // eslint-disable-next-line no-console
         onEdit: (e, action) => console.log(e, action),
       }}
       footer={[
@@ -120,12 +171,73 @@ export default () => (
       ]}
     >
       <ProCard direction="column" ghost gutter={[0, 16]}>
-        <ProCard style={{ height: 200 }} />
-        <ProCard gutter={16} ghost style={{ height: 200 }}>
-          <ProCard colSpan={16} />
-          <ProCard colSpan={8} />
+        <ProCard style={{ minHeight: 200 }} >
+          <Form
+            name="basic"
+            labelCol={{ span: 4 , offset: 0}}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 4, span: 16 }}>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 4, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+
+          <ProTable<TableListItem>
+            columns={columns}
+            request={(params, sorter, filter) => {
+              // 表单搜索项会从 params 传入，传递给后端接口。
+              console.log(params, sorter, filter);
+              return Promise.resolve({
+                data: tableListDataSource,
+                success: true,
+              });
+            }}
+            rowKey="key"
+            pagination={{
+              showQuickJumper: true,
+            }}
+            search={false}
+            dateFormatter="string"
+            headerTitle="表格标题"
+            // toolBarRender={() => [
+            //   <Button key="show">查看日志</Button>,
+            //   <Button key="out">
+            //     导出数据
+            //     <DownOutlined />
+            //   </Button>,
+            //   <Button type="primary" key="primary">
+            //     创建应用
+            //   </Button>,
+            // ]}
+          />
         </ProCard>
+
       </ProCard>
     </PageContainer>
-  </div>
+  // </div>
 );
